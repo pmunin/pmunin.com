9bb36474a24898f42a7d89731da1387645a626f0
pmunin <philipp.munin@gmail.com>
2018-05-18 16:17:22 -0400

    rebuild pages

--- docs/headScript.js
+++ docs/headScript.js
@@ -1,7 +1,7 @@
 //script should be injected in the head of blogger's template HTML
 
 /**
- * 
+ * Renders content of the github source file or gist to current document
  * 
  * @param {string} fileUrl 
  */
@@ -10,10 +10,8 @@ function githubFile(fileUrl)
     //see documentation here: http://gist-it.appspot.com/
     // example: <script src="https://gist-it.appspot.com/https://github.com/pmunin/homepage/blob/master/views/layout.ejs?footer=minimal"></script>
 
-    //console.log("rendering code of github file:"+fileUrl);
-    // if(fileUrl.startsWith(""))
-    // if(fileUrl.indexOf("github.com")==-1)
-    document.write("<script src='https://gist-it.appspot.com/"+fileUrl+"'></script>")
-    // else 
-    //     document.write("<script src='https://gist-it.appspot.com/https://github.com/"+fileUrl+"'></script>")
+    if(fileUrl.indexOf("gist.github.com")>-1)
+        document.write("<script src='"+fileUrl+"'></script>");
+    else
+        document.write("<script src='https://gist-it.appspot.com/"+fileUrl+"'></script>")
 }
