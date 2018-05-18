e7392640ab6dd7f1c97abb84855fd743a7debf82
pmunin <philipp.munin@gmail.com>
2018-05-18 16:04:15 -0400

    rebuild pages

--- headScript.js
+++ headScript.js
@@ -1,10 +1,19 @@
-//script loaded by pmunin.com at loader
+//script should be injected in the head of blogger's template HTML
 
+/**
+ * 
+ * 
+ * @param {string} fileUrl 
+ */
 function githubFile(fileUrl)
 {
     //see documentation here: http://gist-it.appspot.com/
     // example: <script src="https://gist-it.appspot.com/https://github.com/pmunin/homepage/blob/master/views/layout.ejs?footer=minimal"></script>
 
-    console.log("rendering code of github file:"+fileUrl);
-    document.write("<script src='https://gist-it.appspot.com/http://github.com/"+fileUrl+"'></script>")
+    //console.log("rendering code of github file:"+fileUrl);
+    // if(fileUrl.startsWith(""))
+    // if(fileUrl.indexOf("github.com")==-1)
+    document.write("<script src='https://gist-it.appspot.com/"+fileUrl+"'></script>")
+    // else 
+    //     document.write("<script src='https://gist-it.appspot.com/https://github.com/"+fileUrl+"'></script>")
 }
