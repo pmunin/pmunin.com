768f5b20268bf429509756ec739ee312b3501caa
pmunin <philipp.munin@gmail.com>
2018-05-18 15:42:09 -0400

    added head script

---
+++ headScript.js
@@ -0,0 +1,9 @@
+//script loaded by pmunin.com at loader
+
+// https://pmunin.github.io/pmunin.com/util.js
+// <script src="https://gist-it.appspot.com/https://github.com/pmunin/homepage/blob/master/views/layout.ejs?footer=minimal"></script>
+
+function githubFile(fileUrl)
+{
+    console.log("rendering code of github file:"+fileUrl);
+}
