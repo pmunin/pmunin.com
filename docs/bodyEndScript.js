0ab83b71dc82f094ecb140c58dbc827b67c43f0e
pmunin <philipp.munin@gmail.com>
2018-05-22 12:06:34 -0400

    adding search hightlight

--- docs/bodyEndScript.js
+++ docs/bodyEndScript.js
@@ -1,2 +1,14 @@
 //script should be inserted before body tag closes
-console.log("Body end script loaded");
+
+//highlight by parameter
+(function(){
+    let params = getJsonFromUrl();
+    let highlightText = params&&params.highlight;
+    if(!highlightText) return;
+
+    //depends on https://markjs.io/ 
+    // https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/mark.min.js should be loaded in the head of template
+    if(!Mark) return;
+    var markInstance = new Mark("*");
+    markInstance.mark(highlightText, {separateWordSearch:false});
+})();
