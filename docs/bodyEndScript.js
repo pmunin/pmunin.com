2c2a68488e4548ac0110a879b1cd09095a117790
pmunin <philipp.munin@gmail.com>
2018-05-22 13:04:58 -0400

    rebuild pages

--- docs/bodyEndScript.js
+++ docs/bodyEndScript.js
@@ -1,6 +1,6 @@
 //script should be inserted before body tag closes
 
-//?highlight=text - highlight exact text
+//?highlight=text+to+hightlight - highlight exact text (space bars can be replaced with +)
 (function(){
     let params = getJsonFromUrl();
     let highlightText = params&&params.highlight;
@@ -9,6 +9,6 @@
     //depends on https://markjs.io/ 
     // https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/mark.min.js should be loaded in the head of template
     if(!Mark) return;
-    var markInstance = new Mark("*");
+    var markInstance = window.markInstance = new Mark("*");
     markInstance.mark(highlightText, {separateWordSearch:false, iframes:true});
 })();
